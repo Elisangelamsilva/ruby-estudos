fddd734483e919c829e95366ed9e48cb3bf48523
Elisangela <elisangela00silva@gmail.com>
2020-09-30 21:10:59 -0300

    aula 2 e resumo

---
+++ oneBitCode/aulas/aula02.rb
@@ -0,0 +1,27 @@
+# Tipagem dinamica
+variavel01 = 10
+puts variavel01.class
+variavel02 = 10.01
+puts variavel02.class
+variavel03 = true
+puts variavel03.class
+variavel04 = false
+puts variavel04.class
+
+string = 'Hello word!'
+puts string
+puts string.class
+
+bitcode_array = ['position0', 'position1', 'position2', 1, 2, 3]
+puts bitcode_array
+puts bitcode_array.class
+puts bitcode_array[0]
+
+bitcode_symbol = :onebit
+puts bitcode_symbol.class
+puts bitcode_symbol.object_id
+
+onebit_hash = {course: 'ruby', lenguage: 'pt-Br', locale: 'youtube'}
+puts onebit_hash[:course]
+
+
